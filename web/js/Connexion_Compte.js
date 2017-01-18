d2c1327eda4154daf222963c0c6e05546eb044eb
Abdelkarim <Abdelkarim@LAPTOP-20JBQAQ0>
2017-01-18 17:11:09 +0100

    Mise à jour du code Connexion d'un compte

--- web/js/Connexion_Compte.js
+++ web/js/Connexion_Compte.js
@@ -5,11 +5,12 @@ function verifFormConn(f){
     if(mailOk && mdpOK){
         var email = document.getElementById('email').value;
         var mdp = document.getElementById('mdp').value;
-        
+        //var params = email+"/"+mdp;
         var xmlhttp = new XMLHttpRequest();
-        var url = "http://localhost:8080/E-DRIVE/webresources/Utilisateur/Connexion/";
-        xmlhttp.open('GET',url,true);
-        xmlhttp.send('mail=' + email + '&mdp=' + mdp);
+        var url = "http://localhost:8080/E-DRIVE/webresources/Utilisateur/Connexion/"+email+"/"+mdp;
+        xmlhttp.open('GET',url, true);
+        xmlhttp.send(null);
+        xmlhttp.onreadystatechange = function(){ alertContents(xmlhttp); };
 
     }
     else
@@ -17,17 +18,37 @@ function verifFormConn(f){
        alert("Veuillez remplir correctement tous les champs");
     }
 }
-
+ function alertContents(http){
+            //try {
+                if (http.readyState == 4) {
+                   if ( http.status == 200 && http.readyState == XMLHttpRequest.DONE) {
+                        var resp = eval( "(" +  http.responseText + ")");
+                        //var resp = xmlhttp.responseText;
+                        if (resp.mail == email) {
+                        //if (resp.echec != null) {
+                            alert("Bonjour" + resp.dateInscription);
+                       }
+                       else 
+                       {
+                           alert("Le mot de passe et le user n'existe pas.");
+                       }
+                    }
+                }
+           // }
+           // catch( e ) {
+            //    alert("Une exception s'est produite : " + e.description);
+           // }
+        }
 function verif_champConn(champ) {
     if (champ.value == "") {
-        //alert("Le champs "+champ+" n'est pas renseigné");
+        //alert("Le champs "+champ+" n'est pas renseignÃ©");
         surligne(champ, true);
         return false;
     }
     surligne(champ, false);
     return true;
 }
-// Vérification du champs Email
+// VÃ©rification du champs Email
 function verifMailConn(champ){
     var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
     if(!regex.test(champ.value))
